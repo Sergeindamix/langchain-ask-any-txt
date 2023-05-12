969b5773f38213fa4d096c458872498bc25f8455
Sergeindamix <86927266+Sergeindamix@users.noreply.github.com>
2023-05-12 14:50:43 -0600

    Update agentes.py
    update agent prompts

--- agentes.py
+++ agentes.py
@@ -3,7 +3,7 @@ import requests
 from IPython.display import Audio
 import soundfile as sf
 from transformers.tools import HfAgent
-from gtts import gTTS
+
 # InstructorEmbedding 
 from langchain.embeddings import HuggingFaceInstructEmbeddings
 import faiss
@@ -20,17 +20,9 @@ tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModel.from_pretrained(model_name)
 
 def text_downloader(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        # split into chunks    
-        chunks = split_text(response.text)
-        st.write(chunks)
-        
-        
-          
-        return response.text
-    else:
-        return ""
+    response = agent.run(f"Summarize the text given in {url} and read it out loud.")
+    return response.text
+    
 
 
 # Define una función para dividir el texto en trozos de 2048 tokens
@@ -44,14 +36,14 @@ def split_text(text):
   return chunks
 
 
-def text_reader(text):    
-    #audio = agent.run(text) #"Please read out loud the contents of the https://gamma.app/docs/sgx5tfyh0wttucu")
+def text_reader(url):        
+    audio = agent.run(f"Please read out loud the contents of the {url}")
     # Convierte el texto en un archivo de audio
-    tts = gTTS(text=text, lang="es")
-    audio_file = "audio.mp3"
-    tts.save(audio_file)
+    #tts = gTTS(text=text, lang="es")
+    #audio_file = "audio.mp3"
+    #tts.save(audio_file)
     # Reproduce el archivo de audio
-    return Audio(audio_file, autoplay=True)
+    return Audio("speech_converted.wav", autoplay=True)
 
 def promptx():
     # Define el título de la aplicación
@@ -62,18 +54,18 @@ def promptx():
     text = text_downloader(url)
     # Muestra el texto del artículo
     st.write("Texto del artículo:")
-    st.write(text)
+    #st.write(text)
     
 
     
     if st.button("Download Text"):
         text = text_downloader(url)
-        st.write(f"The text is {text}")
+        #st.write(f"The text is {text}")
 
     if st.button("Generate Audio"):
       # Convierte el texto en un archivo de audio y lo reproduce
       st.write("Audio del artículo:")
-      audio = text_reader(text)
+      audio = text_reader(url)
       st.write(audio)
         
         
