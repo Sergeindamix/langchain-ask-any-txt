3c2798c83d4ba419491bad097b7e2d08ef986ce3
Sergeindamix <86927266+Sergeindamix@users.noreply.github.com>
2023-07-28 12:21:01 -0600

    Update sd.py

--- sd.py
+++ sd.py
@@ -1,29 +1,24 @@
-#!pip install replicate
-# get a token: https://replicate.com/account 
-
-
-
+# get a token: https://replicate.com/account
 REPLICATE_API_TOKEN = "r8_brmwdLDHOD3znpSbSdz8OeUQfTLXujD1ttixD"
 import os
+os.environ["REPLICATE_API_TOKEN"] = REPLICATE_API_TOKEN
 
-os.environ["REPLICATE_API_TOKEN"] = REPLICATE_API_TOKEN #"r8_brmwdLDHOD3znpSbSdz8OeUQfTLXujD1ttixD"
 from langchain.llms import Replicate
-from langchain import PromptTemplate, LLMChain
 import streamlit as st
-#from replicate import Replicate
 from PIL import Image
 import requests
 from io import BytesIO
 
-
-
 def sd(input_text):
     st.title("Text to Image Conversion")
 
-    # create replicate model
+    # Text input to get the image dimensions from the user
+    image_dimensions = st.text_input("Enter the image dimensions (e.g., 768x768)", "1024x1024")
+
+    # create replicate model with the specified image dimensions
     model = Replicate(
         model="stability-ai/sdxl:2b017d9b67edd2ee1401238df49d75da53c523f36e363881e057f5dc3ed3c5b2",
-        input={"image_dimensions": "768x768"}
+        input={"image_dimensions": image_dimensions}
     )
 
     # get user input
